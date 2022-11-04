3953d785b710fb10d08898330e1dc10dfa2d0539
CodeAbsolute <68479079+CodeAbsolute@users.noreply.github.com>
2022-11-04 14:14:49 +0530

    Solved Issue 2

--- Controllers/BandController.js
+++ Controllers/BandController.js
@@ -32,7 +32,54 @@ const userSpecificBands = async (req, res) => {
     }
   }
 };
+
+const addBand = async (req, res) => {
+  // finding the band with the entered band name and storing it in findBand variable
+  const findBand = await Band.findOne({ name: req.body.name });
+  // if findBand doesn't exist means there are no bands with the same name in the db
+  // now we can create the new band
+  if (!findBand) {
+    // destructuring the request body
+    const { name, desc, origin, rating } = req.body;
+    // storing the userid from the url
+    const userid = req.params.uid;
+    // getting email of an user from session storage
+    const email = req.session.user.email;
+    // creating a bandObject variable with all the data that need to be passed
+    const bandObject = {
+      name: name,
+      desc: desc,
+      origin: origin,
+      rating: rating,
+      userid: userid,
+      email: email,
+    };
+    console.log("bandObject: ", bandObject);
+    // creating new band by passing bandObject  variable
+    const newBand = new Band(bandObject);
+
+    try {
+      // now saving the new band data into db
+      //  if it gets saved with no error then it will redirect to home page and flash success msg
+      // else throws an error
+      const savedBand = await newBand.save();
+      console.log(savedBand);
+      req.flash("success", `${savedBand.name} is created Successfully!!!`);
+      res.status(201).redirect("/api/home");
+    } catch (err) {
+      req.flash("error", `Couldn't create the band`);
+      res.status(500).redirect("/api/home");
+      // res.status(500).json(err);
+    }
+  } else {
+    // if entered band name from the body already exists then show an error
+    req.flash("error", `Band Name Already Exist`);
+    // res.status(422).redirect(`api/users/user/${req.params.uid}`);
+  }
+};
+
 module.exports = {
+  addBand,
   allBands,
   userSpecificBands,
 };
--- routes/bands.js
+++ routes/bands.js
@@ -5,10 +5,18 @@ const {
 } = require("../Controllers/BandController");
 const Band = require("../models/Bands");
 
+// rendering the add band page
+router.get("/:uid/bands", async (req, res) => {
+  res.render("add-band", { id: req.params.uid });
+});
+
 // GET USER SPECIFIC BANDS
 router.get("/:uid/bands/band/:bid", userSpecificBands);
 
 //GET ALL BANDS
 router.get("/bands/explore", allBands);
+
+//CREATE Band from received data
+router.post("/:uid/bands", addBand);
 
 module.exports = router;
